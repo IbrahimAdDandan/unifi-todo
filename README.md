f55c80051bb68d45dde512dc3ebc74504ed333d1
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2021-06-12 16:48:49 +0300

    environment and base structure...

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Unifi test
+
+## before run
+please rename `./config/environment.js` to `./config/environment.prod.js`
+and set your environment variables in the file.
+
 To run please execute
 `npm install`
 `npm start`
--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,8 @@
 module.exports = {
     port: 3000,
+    database1Name: 'MySQL_DB_NAME',
+    database1Username: 'MySQL_DB_USERNAME',
+    database1Password: 'MySQL_DB_PASS',
+    database1Host: 'localhost',
+    database1Dialect: 'mysql',
 };
